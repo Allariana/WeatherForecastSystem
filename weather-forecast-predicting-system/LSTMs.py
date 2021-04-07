e6b8a3a09ba1635c9fd41002dc47e887b7596ac8
unknown <kinga.sochacka08@gmail.com>
2021-04-07 14:28:45 +0200

    fit LSTMs model

--- weather-forecast-predicting-system/LSTMs.py
+++ weather-forecast-predicting-system/LSTMs.py
@@ -1,6 +1,3 @@
-import math
-
-from keras.activations import sigmoid
 from keras.optimizer_v1 import SGD
 from pandas import read_csv
 from pandas import DataFrame
@@ -8,7 +5,7 @@ from pandas import concat
 from pandas import Series
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
-from keras.layers import Dense, LSTM, Activation
+from keras.layers import Dense, LSTM
 from constants import *
 
 # define contrived series
@@ -20,32 +17,37 @@ values = values.reshape((len(values), 1))
 # train the normalization
 scaler = MinMaxScaler(feature_range=(0, 1))
 scaler = scaler.fit(values)
-# normalize the dataset and print
+# normalize the dataset
 normalized = scaler.transform(values)
+# inverse transform
+# inversed = scaler.inverse_transform(normalized)
+# Preparing Data
 values = DataFrame(DataFrame(normalized).values)
 dataframe = concat([values.shift(1), values], axis=1)
 dataframe.columns = ['t-1', 't']
-data = dataframe.to_numpy()
-data = data.reshape((data.shape[0], data.shape[1], 1))
-# inverse transform and print
-inversed = scaler.inverse_transform(normalized)
-# print(inversed)
+X = dataframe['t-1']
+y = dataframe['t']
+X = X.to_numpy()
+X = X.reshape(1, 4022, 1)
+y = y.to_numpy()
+y = y.reshape(1, 4022, 1)
 # define the model
 model = Sequential()
-model.add(LSTM(5, input_shape=(2, 1)))
+model.add(LSTM(5, input_shape=(4022, 1)))
 model.add(Dense(1))
 algorithm = SGD(lr=0.1, momentum=0.3)
 model.compile(optimizer='sgd', loss='mean_squared_error', metrics=['accuracy'])
 # split data for train and test
-training_dataset_length = math.ceil(len(data) * .75)
-train_data = data[0:training_dataset_length, :]
-x_train = []
-y_train = []
+# training_dataset_length = math.ceil(len(data) * .75)
+# train_data = data[0:training_dataset_length, :]
+# x_train = []
+# y_train = []
 
+# for i in range(10, len(train_data)):
+#     x_train.append(train_data[i-10:i, 0])
+#     y_train.append(train_data[i, 0])
 
-for i in range(10, len(train_data)):
-    x_train.append(train_data[i-10:i, 0])
-    y_train.append(train_data[i, 0])
-
-model.fit(x_train, y_train, batch_size=32, epochs=100)
+model.fit(X, y, batch_size=3500, epochs=100)
+# Evaluate the Model
+loss, accuracy = model.evaluate(X, y)  # accuracy 0.0002
 print()

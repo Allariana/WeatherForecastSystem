f19c401dea771517dcac6a770a382fc24541a1bc
unknown <kinga.sochacka08@gmail.com>
2021-04-11 15:55:48 +0200

    forecast based on 3 days

--- weather-forecast-predicting-system/LSTM_3days.py
+++ weather-forecast-predicting-system/LSTM_3days.py
@@ -44,21 +44,21 @@ values = values.astype('float32')
 scaler = MinMaxScaler(feature_range=(0, 1))
 scaled = scaler.fit_transform(values)
 # frame as supervised learning
-reframed = series_to_supervised(scaled, 1, 1)
+reframed = series_to_supervised(scaled, N_DAYS, 1)
 # drop columns we don't want to predict
-reframed.drop(reframed.columns[[8, 9, 11, 12, 13, 14, 15]], axis=1, inplace=True)
+# reframed.drop(reframed.columns[[8, 9, 11, 12, 13, 14, 15]], axis=1, inplace=True)
+reframed.drop(reframed.columns[[24, 25, 27, 28, 29, 30, 31]], axis=1, inplace=True)
 # reframed.drop(reframed.columns[[3]], axis=1, inplace=True)
 # split into train and test sets
 values = reframed.values
 train = values[:TRAIN_SIZE, :]
 test = values[TRAIN_SIZE:, :]
 # split into input and outputs
-train_X, train_y = train[:, :-1], train[:, -1]
-test_X, test_y = test[:, :-1], test[:, -1]
+train_X, train_y = train[:, :N_OBS], train[:, -N_FEATURES]
+test_X, test_y = test[:, :N_OBS], test[:, -N_FEATURES]
 # reshape input to be 3D [samples, timesteps, features]
-train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
-test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
-print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
+train_X = train_X.reshape((train_X.shape[0], N_DAYS, N_FEATURES))
+test_X = test_X.reshape((test_X.shape[0], N_DAYS, N_FEATURES))
 # design network
 model = Sequential()
 model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
@@ -66,21 +66,16 @@ model.add(Dense(1))
 model.compile(loss='mae', optimizer='adam')
 # fit network
 history = model.fit(train_X, train_y, epochs=50, batch_size=72, validation_data=(test_X, test_y), verbose=2, shuffle=False)
-# plot history
-# pyplot.plot(history.history['loss'], label='train')
-# pyplot.plot(history.history['val_loss'], label='test')
-# pyplot.legend()
-# pyplot.show()
 # make a prediction
 yhat = model.predict(test_X)
-test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
+test_X = test_X.reshape((test_X.shape[0], N_DAYS*N_FEATURES))
 # invert scaling for forecast
-inv_yhat = concatenate((yhat, test_X[:, 1:]), axis=1)
+inv_yhat = concatenate((yhat, test_X[:, -7:]), axis=1)
 inv_yhat = scaler.inverse_transform(inv_yhat)
 inv_yhat = inv_yhat[:, 0]
-# invert scaling for actual
+# # invert scaling for actual
 test_y = test_y.reshape((len(test_y), 1))
-inv_y = concatenate((test_y, test_X[:, 1:]), axis=1)
+inv_y = concatenate((test_y, test_X[:, -7:]), axis=1)
 inv_y = scaler.inverse_transform(inv_y)
 inv_y = inv_y[:, 0]
 # plot forecasts against actual outcomes
@@ -92,4 +87,4 @@ for t in range(100):
 # calculate RMSE
 rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
 print('Test RMSE: %.3f' % rmse)
-print()
+# print()

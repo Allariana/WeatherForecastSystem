29338c1302cb62e435e5818488c9a72239b69e03
unknown <kinga.sochacka08@gmail.com>
2021-04-13 11:24:20 +0200

    script for api service

--- app/src/main/java/TestMain.java
+++ app/src/main/java/TestMain.java
@@ -8,15 +8,15 @@ import java.sql.Timestamp;
 public class TestMain {
 
     OkHttpClient client = new OkHttpClient();
-    String date = "2021-04-06 1:00:00"; //Replace with your value
-    Timestamp timestamp = Timestamp.valueOf(date);
-    // Convert timestamp to long for use
-    long timeParameter = timestamp.getTime();
+//    String date = "2021-04-06 1:00:00"; //Replace with your value
+//    Timestamp timestamp = Timestamp.valueOf(date);
+//    // Convert timestamp to long for use
+//    long timeParameter = timestamp.getTime();
 
     String doGetRequest(String url) throws IOException {
 
         Request request = new Request.Builder()
-                .url("https://community-open-weather-map.p.rapidapi.com/onecall/timemachine?lat=52.21866&lon=20.99746&dt=1617757200")
+                .url("https://community-open-weather-map.p.rapidapi.com/onecall/timemachine?lat=52.21866&lon=20.99746&dt=1618217921")
                 .get()
                 .addHeader("x-rapidapi-key", "2f8a103f4emshe6452ebdc159a90p156155jsn9cd2567b8e5c")
                 .addHeader("x-rapidapi-host", "community-open-weather-map.p.rapidapi.com")
---
+++ weather-forecast-predicting-system/OWMApi.py
@@ -0,0 +1,15 @@
+import http.client
+
+conn = http.client.HTTPSConnection("community-open-weather-map.p.rapidapi.com")
+
+headers = {
+    'x-rapidapi-key': "2f8a103f4emshe6452ebdc159a90p156155jsn9cd2567b8e5c",
+    'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com"
+    }
+
+conn.request("GET", "/onecall/timemachine?lat=52.29958465640118&lon=20.927704121901673&dt=1618217921", headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))

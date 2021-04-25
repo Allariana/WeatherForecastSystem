2224b6497cfb2005f77cbc4b3eae19d1edd18393
unknown <kinga.sochacka08@gmail.com>
2021-04-25 19:47:39 +0200

    predict for actual data

--- weather-forecast-predicting-system/actual_weather_prediction.py
+++ weather-forecast-predicting-system/actual_weather_prediction.py
@@ -1,22 +1,26 @@
 import OWMApi
-import pickle
 from keras.models import load_model
 from joblib import load
 from pandas import DataFrame
+import numpy as np
+from constants import *
 
 actual_data = []
 actual_data = OWMApi.get_actual_weather_data()
 df = DataFrame(actual_data).transpose()
 values = df.values
 values = values.astype('float32')
-# scaler = load('scaler.bin')
-# scaler = pickle.load(open('scaler.pkl', 'rb'))
 scaler = load('scaler.joblib')
-# scaled = scaler.fit_transform(values)
-scaled = scaler.transform(values)
+values_all = values[:, 0:5]
+values_all = scaler.transform(values_all)
+for i in range(5, 21, 5):
+    values_cut = values[:, i:(i+5)]
+    values_cut = scaler.transform(values_cut)
+    values_all = np.concatenate([values_all, values_cut], axis=1)
 
+values_all = values_all.reshape((values_all.shape[0], N_DAYS, N_FEATURES))
 # load model from single file
 model = load_model('lstm_model.h5')
 # make predictions
-# yhat = model.predict(X, verbose=0)
-# print(yhat)
+yhat = model.predict(values_all)
+print(yhat)

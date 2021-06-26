6ac8a879f5b3a317103f361a5e0a58b66e4bcff6
unknown <kinga.sochacka08@gmail.com>
2021-06-26 17:19:40 +0200

    get and print date

--- web-app/src/main/java/com/weather/BaseController.java
+++ web-app/src/main/java/com/weather/BaseController.java
@@ -6,15 +6,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Controller
-public class BaseController extends ScriptCaller{
+public class BaseController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView index(HttpServletRequest request) throws Exception {
-        String temp = callScript();
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+        Date date = new Date(System.currentTimeMillis());
+        Date date2 = new Date(System.currentTimeMillis() + (1000 * 60 * 60 * 24));
+
+        String today = formatter.format(date);
+        String tomorrow = formatter.format(date2);
+
         ModelAndView model = new ModelAndView("index.html");
-        model.addObject("date", temp);
+        model.addObject("today", today);
+        model.addObject("tomorrow", tomorrow);
         return model;
     }
 }
